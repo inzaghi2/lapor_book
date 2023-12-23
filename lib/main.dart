011fd1be8bce2923c28af9f7b3d19b4ce2f976a2
inzaghi2 <inzaghi202@gmail.com>
2023-12-24 00:54:05 +0700

    Tugas 1 (Login & Register)

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lapor_book/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:lapor_book/pages/AddFormPage.dart';
+import 'package:lapor_book/pages/DetailPages.dart';
+import 'package:lapor_book/pages/LoginPage.dart';
+import 'package:lapor_book/pages/RegisterPage.dart';
+import 'package:lapor_book/pages/dashboard/Dashboardpage.dart';
+import 'package:lapor_book/pages/splashpages.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MaterialApp(
+    title: 'Lapor Book',
+    initialRoute: '/',
+    routes: {
+      '/': (context) => const SplashPage(),
+      '/login': (context) => LoginPage(),
+      '/register': (context) => const RegisterPage(),
+      '/dashboard': (context) => const DashboardPage(),
+      '/add': (context) => AddFormPage(),
+      '/detail': (context) => DetailPage(),
+    },
+  ));
+}
---
+++ lib/pages/AddFormPage.dart
@@ -0,0 +1,417 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:image_picker/image_picker.dart';
+
+import 'package:lapor_book/components/styles.dart';
+import 'package:lapor_book/components/vars.dart';
+import 'package:lapor_book/models/laporan.dart';
+import '../components/input_widget.dart';
+import '../components/list_item.dart';
+import '../components/validators.dart';
+import '../models/akun.dart';
+
+class AddFormPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => AddFormState();
+}
+
+class AddFormState extends State<AddFormPage> {
+  final _auth = FirebaseAuth.instance;
+  final _firestore = FirebaseFirestore.instance;
+  final _storage = FirebaseStorage.instance;
+
+  bool _isLoading = false;
+
+  String? judul;
+  String? instansi;
+  String? deskripsi;
+
+  ImagePicker picker = ImagePicker();
+  XFile? file;
+
+  Image imagePreview() {
+    if (file == null) {
+      return Image.asset('assets/istock-default.jpg', width: 180, height: 180);
+    } else {
+      return Image.file(File(file!.path), width: 180, height: 180);
+    }
+  }
+
+  Future<dynamic> uploadDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext) {
+          return AlertDialog(
+            title: Text('Pilih sumber '),
+            actions: [
+              TextButton(
+                onPressed: () async {
+                  XFile? upload =
+                      await picker.pickImage(source: ImageSource.camera);
+
+                  setState(() {
+                    file = upload;
+                  });
+
+                  Navigator.of(context).pop();
+                },
+                child: const Icon(Icons.camera_alt),
+              ),
+              TextButton(
+                onPressed: () async {
+                  XFile? upload =
+                      await picker.pickImage(source: ImageSource.gallery);
+                  setState(() {
+                    file = upload;
+                  });
+
+                  Navigator.of(context).pop();
+                },
+                child: const Icon(Icons.photo_library),
+              ),
+            ],
+          );
+        });
+  }
+
+  Future<Position> getCurrentLocation() async {
+    bool isServiceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!isServiceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permantly denied, we cannot request permissions.');
+    }
+
+    return await Geolocator.getCurrentPosition();
+  }
+
+  Future<String> uploadImage() async {
+    if (file == null) return '';
+
+    String uniqueFilename = DateTime.now().millisecondsSinceEpoch.toString();
+
+    try {
+      Reference dirUpload =
+          _storage.ref().child('upload/${_auth.currentUser!.uid}');
+      Reference storedDir = dirUpload.child(uniqueFilename);
+
+      await storedDir.putFile(File(file!.path));
+
+      return await storedDir.getDownloadURL();
+    } catch (e) {
+      return '';
+    }
+  }
+
+  void addTransaksi(Akun akun) async {
+    setState(() {
+      _isLoading = true;
+    });
+    try {
+      CollectionReference laporanCollection = _firestore.collection('laporan');
+
+      // Convert DateTime to Firestore Timestamp
+      Timestamp timestamp = Timestamp.fromDate(DateTime.now());
+
+      String url = await uploadImage();
+
+      String currentLocation = await getCurrentLocation().then((value) {
+        return '${value.latitude},${value.longitude}';
+      });
+
+      String maps = 'https://www.google.com/maps/place/$currentLocation';
+      final id = laporanCollection.doc().id;
+
+      await laporanCollection.doc(id).set({
+        'uid': _auth.currentUser!.uid,
+        'docId': id,
+        'judul': judul,
+        'instansi': instansi,
+        'deskripsi': deskripsi,
+        'gambar': url,
+        'nama': akun.nama,
+        'status': 'Posted', // posted, process, done
+        'tanggal': timestamp,
+        'maps': maps,
+      }).catchError((e) {
+        throw e;
+      });
+      Navigator.popAndPushNamed(context, '/dashboard');
+    } catch (e) {
+      final snackbar = SnackBar(content: Text(e.toString()));
+      ScaffoldMessenger.of(context).showSnackBar(snackbar);
+    } finally {
+      setState(() {
+        _isLoading = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final akun = ModalRoute.of(context)!.settings.arguments as Akun;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        title:
+            Text('Tambah Laporan', style: headerStyle(level: 3, dark: false)),
+        centerTitle: true,
+      ),
+      body: SafeArea(
+        child: _isLoading
+            ? const Center(
+                child: CircularProgressIndicator(),
+              )
+            : SingleChildScrollView(
+                child: Form(
+                  child: Container(
+                    margin: EdgeInsets.all(40),
+                    child: Column(
+                      children: [
+                        InputLayout(
+                            'Judul Laporan',
+                            TextFormField(
+                                onChanged: (value) => {},
+                                validator: notEmptyValidator,
+                                decoration:
+                                    customInputDecoration("Judul laporan"))),
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 10),
+                          child: imagePreview(),
+                        ),
+                        Container(
+                          width: double.infinity,
+                          margin: EdgeInsets.only(bottom: 10),
+                          child: ElevatedButton(
+                              onPressed: () {
+                                uploadDialog(context);
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Icon(Icons.photo_camera),
+                                  Text(' Foto Pendukung',
+                                      style: headerStyle(level: 3)),
+                                ],
+                              )),
+                        ),
+                        InputLayout(
+                            'Instansi',
+                            DropdownButtonFormField<String>(
+                                decoration: customInputDecoration('Instansi'),
+                                items: dataInstansi.map((e) {
+                                  return DropdownMenuItem(
+                                      child: Text(e), value: e);
+                                }).toList(),
+                                onChanged: (value) => {})),
+                        InputLayout(
+                            "Deskripsi laporan",
+                            TextFormField(
+                              onChanged: (value) => {},
+                              keyboardType: TextInputType.multiline,
+                              minLines: 3,
+                              maxLines: 5,
+                              decoration: customInputDecoration(
+                                  'Deskripsikan semua di sini'),
+                            )),
+                        SizedBox(height: 30),
+                        Container(
+                          width: double.infinity,
+                          child: FilledButton(
+                              style: buttonStyle,
+                              onPressed: () {
+                                addTransaksi(akun);
+                              },
+                              child: Text(
+                                'Kirim Laporan',
+                                style: headerStyle(level: 3, dark: false),
+                              )),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+      ),
+    );
+  }
+}
+
+class AllLaporan extends StatefulWidget {
+  final Akun akun;
+  AllLaporan({super.key, required this.akun});
+
+  @override
+  State<AllLaporan> createState() => _AllLaporanState();
+}
+
+class _AllLaporanState extends State<AllLaporan> {
+  final _firestore = FirebaseFirestore.instance;
+
+  List<Laporan> listLaporan = [];
+  void getTransaksi() async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot =
+          await _firestore.collection('laporan').get();
+
+      setState(() {
+        listLaporan.clear();
+        for (var documents in querySnapshot.docs) {
+          List<dynamic>? komentarData = documents.data()['komentar'];
+
+          List<Komentar>? listKomentar = komentarData?.map((map) {
+            return Komentar(
+              nama: map['nama'],
+              isi: map['isi'],
+            );
+          }).toList();
+
+          listLaporan.add(
+            Laporan(
+              uid: documents.data()['uid'],
+              docId: documents.data()['docId'],
+              judul: documents.data()['judul'],
+              instansi: documents.data()['instansi'],
+              deskripsi: documents.data()['deskripsi'],
+              nama: documents.data()['nama'],
+              status: documents.data()['status'],
+              gambar: documents.data()['gambar'],
+              tanggal: documents['tanggal'].toDate(),
+              maps: documents.data()['maps'],
+              komentar: listKomentar,
+            ),
+          );
+        }
+      });
+    } catch (e) {
+      // final snackbar = SnackBar(content: Text(e.toString()));
+      // ScaffoldMessenger.of(context).showSnackBar(snackbar);
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getTransaksi();
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1 / 1.234,
+            ),
+            itemCount: listLaporan.length,
+            itemBuilder: (context, index) {
+              return ListItem(
+                laporan: listLaporan[index],
+                akun: widget.akun,
+                isLaporanku: false,
+              );
+            }),
+      ),
+    );
+  }
+}
+
+class MyLaporan extends StatefulWidget {
+  final Akun akun;
+  const MyLaporan({super.key, required this.akun});
+
+  @override
+  State<MyLaporan> createState() => _MyLaporanState();
+}
+
+class _MyLaporanState extends State<MyLaporan> {
+  final _firestore = FirebaseFirestore.instance;
+  final _auth = FirebaseAuth.instance;
+
+  List<Laporan> listLaporan = [];
+  void getTransaksi() async {
+    try {
+      QuerySnapshot<Map<String, dynamic>> querySnapshot = await _firestore
+          .collection('laporan')
+          .where('uid', isEqualTo: _auth.currentUser!.uid)
+          .get();
+
+      setState(() {
+        listLaporan.clear();
+        for (var documents in querySnapshot.docs) {
+          List<dynamic>? komentarData = documents.data()['komentar'];
+
+          List<Komentar>? listKomentar = komentarData?.map((map) {
+            return Komentar(
+              nama: map['nama'],
+              isi: map['isi'],
+            );
+          }).toList();
+
+          listLaporan.add(
+            Laporan(
+              uid: documents.data()['uid'],
+              docId: documents.data()['docId'],
+              judul: documents.data()['judul'],
+              instansi: documents.data()['instansi'],
+              deskripsi: documents.data()['deskripsi'],
+              nama: documents.data()['nama'],
+              status: documents.data()['status'],
+              gambar: documents.data()['gambar'],
+              tanggal: documents['tanggal'].toDate(),
+              maps: documents.data()['maps'],
+              komentar: listKomentar,
+            ),
+          );
+        }
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    getTransaksi();
+    return SafeArea(
+      child: Container(
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10,
+              childAspectRatio: 1 / 1.234,
+            ),
+            itemCount: listLaporan.length,
+            itemBuilder: (context, index) {
+              return ListItem(
+                laporan: listLaporan[index],
+                akun: widget.akun,
+                isLaporanku: true,
+              );
+            }),
+      ),
+    );
+  }
+}
